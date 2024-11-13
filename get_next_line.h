4d36a2db6a8b6be650aba7699fc064acda47d7e5
Victor Paulo Da Silva Garcia <victda-s@c2r10p5.42sp.org.br>
2024-11-12 22:29:03 -0300

    correcao .h

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: victda-s <victda-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 11:05:15 by victda-s          #+#    #+#             */
-/*   Updated: 2024/11/12 22:20:26 by victda-s         ###   ########.fr       */
+/*   Updated: 2024/11/12 22:26:30 by victda-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 # define GET_NEXT_LINE_H
 
 # include <fcntl.h>
-# include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
 
